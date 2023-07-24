5b6b70cd9acd4f14ab609ab09699894a3faeb4a8
bdonu0 <bedirahan13@gmail.com>
2023-07-24 18:33:05 +0300

    updated is in the way

---
+++ app/src/main/java/com/example/taskchronos/FocusCounter.kt
@@ -0,0 +1,11 @@
+package com.example.taskchronos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FocusCounter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_focus_counter)
+    }
+}
--- app/src/main/java/com/example/taskchronos/HomePage.kt
+++ app/src/main/java/com/example/taskchronos/HomePage.kt
@@ -1,11 +1,24 @@
 package com.example.taskchronos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+lateinit var counteButton : Button
+
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        counteButton = findViewById(R.id.counter_button)
+
+        counteButton.setOnClickListener {
+            val intent = Intent(baseContext , FocusCounter :: class.java  )
+            startActivity(intent)
+        }
+
     }
 }
