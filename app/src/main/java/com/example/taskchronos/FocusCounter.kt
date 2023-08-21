b8d59745ee2e593267784b63f44bbed4df53d708
bdonu0 <bedirahan13@gmail.com>
2023-08-21 19:11:17 +0300

    change all

--- app/src/main/java/com/example/taskchronos/FocusCounter.kt
+++ app/src/main/java/com/example/taskchronos/FocusCounter.kt
@@ -1,21 +1,22 @@
 package com.example.taskchronos
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-
-private lateinit var counterTextView: TextView
-private lateinit var startButton: Button
-private val handler = Handler()
-private var isCountingDown = true
-private var remainingTimeInMillis = 120 * 60 * 1000
-
+import java.lang.Exception
 
 
 class FocusCounter : AppCompatActivity() {
+
+    private lateinit var counterTextView: TextView
+    private lateinit var startButton: Button
+    private val handler = Handler()
+    private var isCountingDown = true
+    private var remainingTimeInMillis = 120 * 60 * 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,62 +27,46 @@ class FocusCounter : AppCompatActivity() {
         updateCounterText()
     }
 
-    private val countDown = object : Runnable {
+    private val countRunnable = object : Runnable {
         override fun run() {
             if (isCountingDown) {
                 remainingTimeInMillis -= 1000
                 if (remainingTimeInMillis <= 0) {
                     remainingTimeInMillis = 0
-                    isCountingDown = false
+                    stopCounter()
                 }
                 updateCounterText()
-                handler.postDelayed(this, 1000)
-            }
-        }
-    }
-
-    private val countUp = object : Runnable {
-        override fun run() {
-            if (!isCountingDown) {
+            } else {
                 remainingTimeInMillis += 1000
                 if (remainingTimeInMillis >= 120 * 60 * 1000) {
                     remainingTimeInMillis = 120 * 60 * 1000
                 }
                 updateCounterText()
-                handler.postDelayed(this, 1000)
             }
+            handler.postDelayed(this, 1000)
         }
     }
 
-    fun startCounter(view: View) {
-        handler.removeCallbacks(countUp)
-        if (isCountingDown) {
-            handler.postDelayed(countDown, 1000)
-        } else {
-            handler.removeCallbacks(countDown) // Sayaç durduğunda, geri sayımı durdurun
-            handler.removeCallbacks(countUp) // Sayaç durduğunda, sayımı durdurun
-            handler.postDelayed(countUp, 1000)
-        }
-        isCountingDown = !isCountingDown // Durumu tersine çeviriyoruz (Başlama -> Durdurma, Durdurma -> Başlama)
-
-    }
-
-    fun stopCounter(view: View) {
-        handler.removeCallbacks(countDown)
-        handler.removeCallbacks(countUp)
-    }
-
-    fun toggleCountingMode(view: View) {
+    fun toggleCounter(view: View) {
         isCountingDown = !isCountingDown
+        startButton.text = if (isCountingDown) "Pause" else "Play"
         if (isCountingDown) {
-            remainingTimeInMillis = 120 * 60 * 1000
+            startCounter()
         } else {
-            remainingTimeInMillis = 0
+            stopCounter()
         }
-        updateCounterText()
-       // startButton.text = if (isCountingDown) "Başla" else "Durdur" // Sayacı değiştirme durumunda buton metnini uygun bir şekilde güncelliyoruz
-        if(isCountingDown) startButton.visibility = View.VISIBLE else  counterTextView.visibility = View.VISIBLE
     }
+
+    private fun startCounter() {
+        handler.removeCallbacks(countRunnable)
+        handler.postDelayed(countRunnable, 1000)
+    }
+
+    private fun stopCounter() {
+        handler.removeCallbacks(countRunnable)
+    }
+
+    // ... Diğer işlevler burada yer alır
 
     private fun updateCounterText() {
         val minutes = (remainingTimeInMillis / (1000 * 60)).toInt()
@@ -90,23 +75,9 @@ class FocusCounter : AppCompatActivity() {
         counterTextView.text = timeText
     }
 
-    fun resetCounter(view: View) {
-        handler.removeCallbacks(countDown) // Eğer sayaç çalışıyorsa durduralım
-        handler.removeCallbacks(countUp)
-
-        isCountingDown = true // Sayaç varsayılan olarak geri sayıma başlasın
-        remainingTimeInMillis = 120 * 60 * 1000 // Varsayılan süreyi 120 dakika olarak ayarlayalım
-        updateCounterText()
-
-
-        if (isCountingDown) {
-            handler.postDelayed(countDown, 1000) // Sayaç geri sayımı başlatalım
-           // startButton.text = "Durdur" // Başlatma durumuna döndürerek sayaç başlasın
-
-        } else {
-            //startButton.text = "Başla" // Durma durumunda buton metnini "Başla" olarak güncelliyoruz
-            handler.postDelayed(countUp, 1000) // Sayaç sayımı başlatalım
-        }
-
+    override fun onDestroy() {
+        super.onDestroy()
+        stopCounter()
     }
 }
+
